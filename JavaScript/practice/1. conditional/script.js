790ee5d3b890cac379b946ca34094b3101326c4a
felix <dev.felix0131@gmail.com>
2023-10-03 16:23:10 +0900

    docs: add conditional practice

---
+++ JavaScript/practice/1. conditional/script.js
@@ -0,0 +1,33 @@
+console.log("Before");
+
+if (1 + 2 === 2) {
+  console.log("Hello World");
+}
+
+console.log("After");
+
+console.log("-----------");
+
+console.log("Before");
+
+if (1 + 1 === 2) {
+  console.log("Hello World - if");
+} else if (1 + 2 === 2) {
+  console.log("Hello World - else if");
+}
+
+console.log("After");
+
+console.log("-----------");
+
+console.log("Before");
+
+if (1 + 1 === 2) {
+  console.log("Hello World - if");
+} else if (1 + 2 === 2) {
+  console.log("Hello World - else if");
+} else {
+  console.log("Hello World - else");
+}
+
+console.log("After");
