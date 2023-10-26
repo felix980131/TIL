2e86c5303dbf70255ef1e107d8a661a8c3c64f2c
felix <dev.felix0131@gmail.com>
2023-10-26 22:14:10 +0900

    docs: add React event practice

--- React/practice/1. basic/myfirstapp/src/App.js
+++ React/practice/1. basic/myfirstapp/src/App.js
@@ -1,23 +1,10 @@
 import "./App.css";
-import ColorList from "./ColorList";
-import ShoppingList from "./ShoppingList";
-
-const data = [
-  {
-    id: 1,
-    item: "eggs",
-    quantity: 12,
-    completed: false,
-  },
-  { id: 2, item: "milk", quantity: 1, completed: true },
-  { id: 3, item: "chicken", quantity: 4, completed: false },
-  { id: 4, item: "carrots", quantity: 6, completed: true },
-];
+import Clicker from "./Clicker";
 
 function App() {
   return (
     <div className="App Die">
-      <ShoppingList items={data} />
+      <Clicker />
     </div>
   );
 }
