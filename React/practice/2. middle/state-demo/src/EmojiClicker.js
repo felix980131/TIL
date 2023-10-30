9ed2cf5544dd4929711c29511085c462d7447f01
felix <dev.felix0131@gmail.com>
2023-10-30 23:42:24 +0900

    docs: add React state array delete practice

--- React/practice/2. middle/state-demo/src/EmojiClicker.js
+++ React/practice/2. middle/state-demo/src/EmojiClicker.js
@@ -1,18 +1,33 @@
+import { v4 as uuid } from "uuid";
 import { useState } from "react";
 
 function EmojiClicker() {
-  const [emojis, setEmojis] = useState(["😀"]);
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😀" }]);
 
   const addEmoji = () => {
     setEmojis((oldEmojis) => {
-      return [...oldEmojis, "🐶"];
+      return [...oldEmojis, { id: uuid(), emoji: "🐶" }];
+    });
+  };
+
+  const deleteEmoji = (id) => {
+    setEmojis((oldEmojis) => {
+      return oldEmojis.filter((e) => e.id != id);
     });
   };
 
   return (
     <div>
       {emojis.map((e) => (
-        <span style={{ fontSize: "4rem" }}>{e}</span>
+        <span
+          key={e.id}
+          style={{ fontSize: "4rem" }}
+          onClick={() => {
+            deleteEmoji(e.id);
+          }}
+        >
+          {e.emoji}
+        </span>
       ))}
       <button onClick={addEmoji}>Add Emoji</button>
     </div>
