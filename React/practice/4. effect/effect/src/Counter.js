53c49352d6a39b51cae90ff3f2440fb23e6d1070
felix <dev.felix0131@gmail.com>
2023-11-04 00:08:30 +0900

    docs: add React useEffect practice

---
+++ React/practice/4. effect/effect/src/Counter.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
+  useEffect(
+    function myEffect() {
+      console.log("My Effect");
+    },
+    [count]
+  );
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>+1</button>
+      <p>name : {name}</p>
+      <input value={name} onChange={handleChange}></input>
+    </div>
+  );
+}
+
+export default Counter;
