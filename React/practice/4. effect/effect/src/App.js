5a78d34e6563034692876361cce0151d52f3495e
felix <dev.felix0131@gmail.com>
2023-11-04 00:55:37 +0900

    docs: add React 초기 api 데이터 practice

--- React/practice/4. effect/effect/src/App.js
+++ React/practice/4. effect/effect/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Counter from "./Counter";
+import QuoteFetcher from "./QuoteFetcher";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <QuoteFetcher />
     </div>
   );
 }
