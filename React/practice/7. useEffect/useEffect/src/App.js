d245a9ba9024fdb1766d6ba166ddc5cd02c6f207
felix <dev.felix0131@gmail.com>
2023-12-13 00:18:02 +0900

    docs: update React practice useEffect localStorage

--- React/practice/7. useEffect/useEffect/src/App.js
+++ React/practice/7. useEffect/useEffect/src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  useEffect(() => {
+    // 로컬 스토리지에 있는 데이터를 확인한 후, 데이터가 LOGGED_IN하면 로그인 상태로 전환
+    const storedLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (storedLoggedIn === "LOGGED_IN") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    // 로컬 스토리지에 데이터 저장
+    localStorage.setItem("isLoggedIn", "LOGGED_IN");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    // 로컬 스토리지 데이터 삭제
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
