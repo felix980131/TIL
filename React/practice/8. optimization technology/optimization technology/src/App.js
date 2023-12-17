5d7eca6b81616914a81dcb4913e304b8a7a79cd4
felix <dev.felix0131@gmail.com>
2023-12-17 23:01:40 +0900

    docs: update React practice optimization useCallback dependency array

--- React/practice/8. optimization technology/optimization technology/src/App.js
+++ React/practice/8. optimization technology/optimization technology/src/App.js
@@ -1,21 +1,29 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import "./App.css";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/UI/Button/Demo/DemoOuput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING");
 
   const toggleHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph}></DemoOutput>
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleHandler}>Show Paragraph</Button>
     </div>
   );
