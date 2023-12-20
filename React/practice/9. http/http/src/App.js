f4064f96fab99f77f2a70a22589dbde8e04c9c52
felix <dev.felix0131@gmail.com>
2023-12-20 23:38:10 +0900

    docs: update React practice http add loading state

--- React/practice/9. http/http/src/App.js
+++ React/practice/9. http/http/src/App.js
@@ -4,24 +4,12 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
-
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMovieHandler() {
+    setIsLoading(true);
+
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -35,6 +23,8 @@ function App() {
     });
 
     setMovies(transformedMovies);
+
+    setIsLoading(false);
   }
 
   return (
@@ -43,7 +33,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length == 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
