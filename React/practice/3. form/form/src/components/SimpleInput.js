ecd423d656a121221a03cf6eb06137baf4b3ea68
felix <dev.felix0131@gmail.com>
2023-12-26 00:03:12 +0900

    docs: update React practice form add validation

--- React/practice/3. form/form/src/components/SimpleInput.js
+++ React/practice/3. form/form/src/components/SimpleInput.js
@@ -1,9 +1,51 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnterName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const nameInputChangeHanlder = (event) => {
+    setEnterName(event.target.value);
+  };
+
+  const formSubmissionHandler = (evnet) => {
+    evnet.preventDefault();
+
+    if (enteredName === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // 바닐라 자바스크립트 코드로 DOM을 조작하는 것은 지양해야 한다.
+    // nameInputRef.current.value = "";
+
+    setEnterName("");
+  };
+
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHanlder}
+          value={enteredName}
+        />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
