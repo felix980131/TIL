14f434837e2d9c105b26cb7148bbf4af01ffd1f3
felix <dev.felix0131@gmail.com>
2023-12-31 16:51:42 +0900

    docs: update React practice redux using redux

--- React/practice/11. redux/redux/src/components/Counter.js
+++ React/practice/11. redux/redux/src/components/Counter.js
@@ -1,12 +1,29 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // useSelector는 리덕스 저장소를 자동으로 구독해준다.
+  const counter = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
