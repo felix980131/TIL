b84b16cf43d47ea84cbc563059688394af1f820c
felix <dev.felix0131@gmail.com>
2024-01-02 23:04:51 +0900

    docs: update React practice redux redux many state handler

--- React/practice/11. redux/redux/src/components/Counter.js
+++ React/practice/11. redux/redux/src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   // useSelector는 리덕스 저장소를 자동으로 구독해준다.
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   const dispatch = useDispatch();
 
@@ -19,12 +20,14 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increment5Handler}>Increment by 5</button>
