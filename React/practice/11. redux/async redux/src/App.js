01e486aa9085bd889b46ad0278ee22f934f7701d
felix <dev.felix0131@gmail.com>
2024-01-05 01:10:14 +0900

    docs: update React practice redux async redux add async

--- React/practice/11. redux/async redux/src/App.js
+++ React/practice/11. redux/async redux/src/App.js
@@ -1,16 +1,79 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  const dispatch = useDispatch();
+
+  // cart가 변경될 때마다 비동기 코드를 실행시킴
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data",
+        })
+      );
+      // 비동기 코드
+      // const response = fetch("https://www.naver.com/");
+
+      // if (!response.ok) {
+      //   throw new Error("error");
+      // }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    // 초기에는 비동기 코드가 실행하지 못하도록 함
+    if (isInitial) {
+      isInitial = !isInitial;
+
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sent cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        ></Notification>
+      )}
+
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- React/practice/11. redux/async redux/src/components/Shop/ProductItem.js
+++ React/practice/11. redux/async redux/src/components/Shop/ProductItem.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 import { cartActions } from "../../store/cart-slice";
@@ -7,8 +7,34 @@ const ProductItem = (props) => {
   const { title, price, description, id } = props;
 
   const dispatch = useDispatch();
+  // const cart = useSelector((state) => state.cart);
 
   const addCartHandler = () => {
+    // const newTotalQuantity = cart.totalQuantity + 1;
+    // const updatedItems = cart.items.slice();
+    // const existingItem = updatedItems.find((item) => item.id === id);
+    // if (!existingItem) {
+    //   const updatedItem = { ...existingItem };
+    //   updatedItem.quantity++;
+    //   updatedItem.price = updatedItem.price + price;
+    //   const existingItemIndex = updatedItems.findIndex(
+    //     (item) => item.id === id
+    //   );
+    //   updatedItem[existingItemIndex] = updatedItem;
+    // } else {
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     name: title,
+    //   });
+    // }
+    // const newCart = {
+    //   totalQuantity: newTotalQuantity,
+    //   items: updatedItems,
+    // };
+    // dispatch(cartActions.replaceCart(newCart));
     dispatch(
       cartActions.addItemToCart({
         id,
