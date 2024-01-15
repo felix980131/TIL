f4c016c64763cb827144fd41989d2448ea448cbb
felix <dev.felix0131@gmail.com>
2024-01-15 23:29:52 +0900

    docs: update React practice route using useNavigate

--- React/practice/12. route/route/src/components/Home.js
+++ React/practice/12. route/route/src/components/Home.js
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigationHandler() {
+    navigate("/product");
+  }
+
   return (
     <>
       <div>My Home Page</div>
       <p>
         Go to <Link to="/product">the list of products</Link>.
+      </p>
+      <p>
+        <button onClick={navigationHandler}>Navigate</button>
       </p>
     </>
   );
