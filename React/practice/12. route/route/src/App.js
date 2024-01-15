a229a8c2785a523377196e7604c2c6165f4ef3d0
felix <dev.felix0131@gmail.com>
2024-01-15 22:43:59 +0900

    docs: update React practice route using another router

--- React/practice/12. route/route/src/App.js
+++ React/practice/12. route/route/src/App.js
@@ -1,17 +1,31 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import HomePage from "./components/Home";
 import ProductsPage from "./components/Products";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/product",
-    element: <ProductsPage />,
-  },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/product" element={<ProductsPage />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomePage />,
+//   },
+//   {
+//     path: "/product",
+//     element: <ProductsPage />,
+//   },
+// ]);
 
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
