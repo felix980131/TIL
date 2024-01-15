9ac7b1bcd9a923f7186d86fdb026f749d227f795
felix <dev.felix0131@gmail.com>
2024-01-15 23:00:24 +0900

    docs: update React practice route using Outlet

--- React/practice/12. route/route/src/App.js
+++ React/practice/12. route/route/src/App.js
@@ -6,26 +6,33 @@ import {
 } from "react-router-dom";
 import HomePage from "./components/Home";
 import ProductsPage from "./components/Products";
+import RootLayout from "./components/RootLayout";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/product" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/product" element={<ProductsPage />} />
+//   </Route>
+// );
 
-const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter(routeDefinitions);
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage />,
-//   },
-//   {
-//     path: "/product",
-//     element: <ProductsPage />,
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/product",
+        element: <ProductsPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
