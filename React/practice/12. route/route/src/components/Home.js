0f439148e5b75728a0be0fdff3bab84e9315af5a
felix <dev.felix0131@gmail.com>
2024-01-15 22:49:17 +0900

    docs: update React practice route using Link

--- React/practice/12. route/route/src/components/Home.js
+++ React/practice/12. route/route/src/components/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <div>My Home Page</div>;
+  return (
+    <>
+      <div>My Home Page</div>
+      <p>
+        Go to <Link to="/product">the list of products</Link>.
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
