9d5c36c436d430c5362d60b258686eb5161eae1b
felix <dev.felix0131@gmail.com>
2024-01-16 23:33:40 +0900

    docs: update React practice route using index

--- React/practice/12. route/route/src/App.js
+++ React/practice/12. route/route/src/App.js
@@ -26,7 +26,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "/",
+        index: true,
+        path: "",
         element: <HomePage />,
       },
       {
