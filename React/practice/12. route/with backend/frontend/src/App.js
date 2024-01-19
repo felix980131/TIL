73269e69f80a4c88e82004ba9f894f88ebe2b455
felix <dev.felix0131@gmail.com>
2024-01-19 23:28:40 +0900

    docs: update React practice route change loader function position

--- React/practice/12. route/with backend/frontend/src/App.js
+++ React/practice/12. route/with backend/frontend/src/App.js
@@ -25,7 +25,7 @@ import HomePage from "./pages/Home";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import RootLayout from "./pages/Root";
 import EventRootPage from "./pages/EventRoot";
 
@@ -45,16 +45,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch("http://localhost:8080/events");
-
-              if (!response.ok) {
-              } else {
-                const resData = await response.json();
-
-                return resData.events;
-              }
-            },
+            loader: eventsLoader,
           },
           {
             path: ":id",
