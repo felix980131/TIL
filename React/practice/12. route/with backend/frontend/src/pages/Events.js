e55afa2d24168f2df237eb5e42d236b22b0b4e8d
felix <dev.felix0131@gmail.com>
2024-01-19 23:21:04 +0900

    docs: update React practice route using loader

--- React/practice/12. route/with backend/frontend/src/pages/Events.js
+++ React/practice/12. route/with backend/frontend/src/pages/Events.js
@@ -1,24 +1,10 @@
-import { Link } from "react-router-dom";
-
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Some Event1" },
-  { id: "e2", title: "Some Event2" },
-  { id: "e3", title: "Some Event3" },
-];
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  return (
-    <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  const events = useLoaderData();
+
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
