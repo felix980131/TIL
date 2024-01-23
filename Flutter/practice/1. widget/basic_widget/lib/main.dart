3e6cb81899b8621a0e4b6072d2b5bc2ce6f62cf3
felix <dev.felix0131@gmail.com>
2024-01-23 23:31:50 +0900

    docs: update Flutter practice basic widget Scaffold

--- Flutter/practice/1. widget/basic_widget/lib/main.dart
+++ Flutter/practice/1. widget/basic_widget/lib/main.dart
@@ -11,7 +11,28 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        // 앱 상단 바
+        appBar: AppBar(
+          title: Text('This is App bar!'), // 상단바 타이틀
+          centerTitle: false, // 타이틀 가운데 정렬의 적용 유무
+          // 오른쪽에 위치할 위젯
+          actions: [
+            IconButton(
+                onPressed: () {
+                  print("clicked!");
+                },
+                icon: Icon(Icons.home)),
+            Icon(Icons.play_arrow)
+          ],
+        ),
+        // 앱의 기본적인 내용
         body: Text('Hello World!'),
+        // 플로팅 버튼
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print("clicked floationg action button!");
+            },
+            child: Icon(Icons.bug_report)),
       ),
     );
   }
