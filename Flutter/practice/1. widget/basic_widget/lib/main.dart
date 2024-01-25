3ae85218bcaee218a8eec4f65e400e7621dbf70a
felix <dev.felix0131@gmail.com>
2024-01-25 23:11:36 +0900

    docs: update Flutter practice basic widget Flexible & Expanded

--- Flutter/practice/1. widget/basic_widget/lib/main.dart
+++ Flutter/practice/1. widget/basic_widget/lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Widget을 상하좌우로 배치'),
+          title: Text('Widget을 비율로 배치'),
         ),
         body: Body(),
       ),
@@ -25,61 +25,50 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal, // 스크롤 방향
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start, // 주축 정렬
-        children: [
-          Container(
-            width: 100,
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 200,
+          color: Colors.red,
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.blue,
             height: 100,
-            color: Colors.grey,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
           ),
-          Container(
-            width: 100,
+        ),
+        Flexible(
+          child: Container(
+            color: Colors.yellow,
             height: 100,
-            color: Colors.grey,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
           ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.grey,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.grey,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.grey,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.grey,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.grey,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.grey,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-        ],
-      ),
+        ),
+        // Flexible(
+        //   flex: 1,
+        //   child: Container(
+        //     color: Colors.red,
+        //   ),
+        // ),
+        // Flexible(
+        //   flex: 2,
+        //   child: Container(
+        //     color: Colors.blue,
+        //   ),
+        // ),
+        // Flexible(
+        //   flex: 3,
+        //   child: Container(
+        //     color: Colors.yellow,
+        //   ),
+        // ),
+        // Flexible(
+        //   flex: 4,
+        //   child: Container(
+        //     color: Colors.brown,
+        //   ),
+        // ),
+      ],
     );
   }
 }
