eff171d1ddfb103908858ef011394514d4187a96
felix <dev.felix0131@gmail.com>
2024-01-31 00:05:07 +0900

    docs: update Flutter practice rock_paper_scissors user_input

--- Flutter/practice/1. widget/rock_paper_scissors/lib/game/view/user_input.dart
+++ Flutter/practice/1. widget/rock_paper_scissors/lib/game/view/user_input.dart
@@ -4,14 +4,31 @@ import 'package:rock_paper_scissors/game/input_card.dart';
 
 class UserInput extends StatelessWidget {
   final bool isDone;
+  final InputType? userInput;
   final Function(InputType) callback;
 
-  const UserInput({super.key, required this.isDone, required this.callback});
+  const UserInput(
+      {super.key,
+      required this.isDone,
+      this.userInput,
+      required this.callback});
 
   @override
   Widget build(BuildContext context) {
     if (isDone) {
-      return Row();
+      return Row(
+        children: [
+          Expanded(child: SizedBox.shrink()),
+          Expanded(
+            child: Center(
+              child: InputCard(
+                child: Image.asset(userInput!.path),
+              ),
+            ),
+          ),
+          Expanded(child: SizedBox.shrink()),
+        ],
+      );
     }
 
     return Row(
