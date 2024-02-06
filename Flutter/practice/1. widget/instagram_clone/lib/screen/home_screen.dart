fc0b034b7bd3ae08360b541b7e04b398b9f9f840
felix <dev.felix0131@gmail.com>
2024-02-06 23:29:53 +0900

    docs: update Flutter practice instagram_clone home_screen.dart FeedItem

--- Flutter/practice/1. widget/instagram_clone/lib/screen/home_screen.dart
+++ Flutter/practice/1. widget/instagram_clone/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -57,11 +58,122 @@ class UserStory extends StatelessWidget {
   }
 }
 
+class FeedData {
+  final String userName;
+  final int likeCount;
+  final String content;
+
+  FeedData({
+    required this.userName,
+    required this.likeCount,
+    required this.content,
+  });
+}
+
+final feedDataList = [
+  FeedData(userName: "user1", likeCount: 15, content: "Content1"),
+  FeedData(userName: "user2", likeCount: 61, content: "Content2"),
+  FeedData(userName: "user3", likeCount: 15, content: "Content3"),
+  FeedData(userName: "user4", likeCount: 94, content: "Content4"),
+  FeedData(userName: "user5", likeCount: 96, content: "Content5"),
+  FeedData(userName: "user6", likeCount: 16, content: "Content6"),
+];
+
 class FeedList extends StatelessWidget {
   const FeedList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: feedDataList.length,
+      itemBuilder: (context, index) {
+        return FeedItem(feedData: feedDataList[index]);
+      },
+    );
+  }
+}
+
+class FeedItem extends StatelessWidget {
+  final FeedData feedData;
+
+  const FeedItem({super.key, required this.feedData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.blue.shade300, shape: BoxShape.circle),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(feedData.userName)
+                ],
+              ),
+              Icon(Icons.more_vert),
+            ],
+          ),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          width: double.infinity,
+          height: 280,
+          color: Colors.indigo.shade300,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.favorite_outline)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(CupertinoIcons.chat_bubble)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(CupertinoIcons.paperplane)),
+                ],
+              ),
+              IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bookmark)),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Text("좋아요 ${feedData.likeCount}개",
+              style: TextStyle(fontWeight: FontWeight.bold)),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 4),
+          child: RichText(
+            text: TextSpan(style: TextStyle(color: Colors.black), children: [
+              TextSpan(
+                text: feedData.userName,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                text: feedData.content,
+              ),
+            ]),
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        )
+      ],
+    );
   }
 }
