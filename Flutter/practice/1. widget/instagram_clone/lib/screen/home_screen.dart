0ba4b0df7fc763ced8718ccda15f6f8c955c3484
felix <dev.felix0131@gmail.com>
2024-02-06 23:11:02 +0900

    docs: update Flutter practice instagram_clone home_screen.dart

--- Flutter/practice/1. widget/instagram_clone/lib/screen/home_screen.dart
+++ Flutter/practice/1. widget/instagram_clone/lib/screen/home_screen.dart
@@ -5,6 +5,63 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Icon(Icons.home, size: 100));
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        StoryArea(),
+        FeedList(),
+      ],
+    ));
+  }
+}
+
+class StoryArea extends StatelessWidget {
+  const StoryArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children:
+              List.generate(10, (index) => UserStory(userName: "User${index}")),
+        ));
+  }
+}
+
+class UserStory extends StatelessWidget {
+  final String userName;
+
+  const UserStory({
+    super.key,
+    required this.userName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+      child: Column(
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+            decoration: BoxDecoration(
+                color: Colors.blue.shade300, shape: BoxShape.circle),
+          ),
+          Text(userName)
+        ],
+      ),
+    );
+  }
+}
+
+class FeedList extends StatelessWidget {
+  const FeedList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
