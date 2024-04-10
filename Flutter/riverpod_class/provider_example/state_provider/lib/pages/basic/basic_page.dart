d45ec7706400beb41f255d2f413b910473ef3ac4
felix <dev.felix0131@gmail.com>
2024-04-10 23:27:38 +0900

    docs: update Flutter provider_example state_provider using StateProvider

--- Flutter/riverpod_class/provider_example/state_provider/lib/pages/basic/basic_page.dart
+++ Flutter/riverpod_class/provider_example/state_provider/lib/pages/basic/basic_page.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_provider/pages/basic/basic_provider.dart';
 
 class BasicPage extends ConsumerWidget {
   const BasicPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<int>(counterProvider, (previous, next) {
+      if (next == 3) {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text("counter: $next"),
+              );
+            });
+      }
+    });
+
+    final value = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('StateProvider'),
       ),
-      body: const Center(
-        child: Text('StateProvider'),
+      body: Center(
+        child: Text(
+          '$value',
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final val = ref.read(counterProvider.notifier);
+          val.state++;
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
