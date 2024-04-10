535d7c1ce119dee01aeb7a2734c61c1f30347b01
felix <dev.felix0131@gmail.com>
2024-04-10 22:04:07 +0900

    docs: update Flutter provider_example basic using autoDispose+family

---
+++ Flutter/riverpod_class/provider_example/basic/provider/lib/pages/auto_dispose_family/auto_dispose_family_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final autoDisposeFamilyHelloProvider =
+    Provider.autoDispose.family<String, String>((ref, name) {
+  ref.onDispose(() {
+    print("[autoDisposeFamilyHelloProvider $name] disposed");
+  });
+
+  return "$name Hello";
+});
