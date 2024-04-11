14cd34c31af4b9b9d78609e3b15f386f5a62614f
felix980131 <dev.felix0131@gmail.com>
2024-04-11 11:51:00 +0900

    docs: update Flutter provider_example state_provider using StateProvider with autoDispose

--- Flutter/riverpod_class/provider_example/state_provider/lib/pages/auto_dispose/auto_dispose_page.dart
+++ Flutter/riverpod_class/provider_example/state_provider/lib/pages/auto_dispose/auto_dispose_page.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_provider/pages/auto_dispose/auto_dispose_provider.dart';
 
 class AutoDisposePage extends ConsumerWidget {
   const AutoDisposePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final value = ref.watch(autoDisposeAgeProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('AutoDisposeStateProvider'),
       ),
-      body: const Center(
-        child: Text('AutoDisposeStateProvider'),
+      body: Center(
+        child: Text(
+          '$value',
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // update 메소드를 이용하여 Provider 상태 업데이트
+          ref.read(autoDisposeCounterProvider.notifier).update((state) {
+            return state + 10;
+          });
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ Flutter/riverpod_class/provider_example/state_provider/lib/pages/auto_dispose/auto_dispose_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auto_dispose_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$autoDisposeAgeHash() => r'bb9d326c5e9179dd0f920ab0fb94165c5f53d839';
+
+/// See also [autoDisposeAge].
+@ProviderFor(autoDisposeAge)
+final autoDisposeAgeProvider = AutoDisposeProvider<String>.internal(
+  autoDisposeAge,
+  name: r'autoDisposeAgeProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$autoDisposeAgeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AutoDisposeAgeRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
