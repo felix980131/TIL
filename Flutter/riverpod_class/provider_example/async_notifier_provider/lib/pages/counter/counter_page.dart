c1ecc08e198301a914fcd4e4d5dd2acb73c566c9
felix <dev.felix0131@gmail.com>
2024-04-15 23:34:07 +0900

    docs: update Flutter provider_example async_notifier_provider using AsyncNotifierProvider

--- Flutter/riverpod_class/provider_example/async_notifier_provider/lib/pages/counter/counter_page.dart
+++ Flutter/riverpod_class/provider_example/async_notifier_provider/lib/pages/counter/counter_page.dart
@@ -1,3 +1,4 @@
+import 'package:async_notifier_provider/pages/counter/counter_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,12 +7,67 @@ class CounterPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterProvider);
+    print(counter);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter'),
       ),
-      body: const Center(
-        child: Text('Counter'),
+      body: Center(
+        child: counter.when(
+            skipLoadingOnRefresh: false,
+            data: (counter) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "$counter",
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      FloatingActionButton(
+                        heroTag: 'decrement',
+                        onPressed: () {
+                          ref.read(counterProvider.notifier).decrement();
+                        },
+                        child: const Icon(Icons.remove),
+                      ),
+                      FloatingActionButton(
+                        heroTag: 'increment',
+                        onPressed: () {
+                          ref.read(counterProvider.notifier).increment();
+                        },
+                        child: const Icon(Icons.add),
+                      )
+                    ],
+                  )
+                ],
+              );
+            },
+            error: (e, st) => Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      e.toString(),
+                      style: Theme.of(context).textTheme.titleLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    OutlinedButton(
+                        onPressed: () {
+                          ref.invalidate(counterProvider);
+                        },
+                        child: Text(
+                          "Refresh",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ))
+                  ],
+                ),
+            loading: () => const CircularProgressIndicator()),
       ),
     );
   }
