86f6fa5db371c1b8002023d9e773d18abf54ecd0
felix <dev.felix0131@gmail.com>
2024-04-22 23:30:02 +0900

    docs: update Flutter scopes optimize_rebuilds before optimize

--- Flutter/riverpod_class/scopes/optimize_rebuilds/lib/pages/show_items.dart
+++ Flutter/riverpod_class/scopes/optimize_rebuilds/lib/pages/show_items.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:optimize_rebuilds/items/item_list_provider.dart';
 
 class ShowItems extends ConsumerWidget {
   const ShowItems({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text('ShowItems'),
+    final items = ref.watch(itemListProvider);
+
+    return ListView(
+      children: [for (final item in items) EachItem(item: item)],
+    );
+  }
+}
+
+class EachItem extends ConsumerWidget {
+  final String item;
+  const EachItem({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    print('building $item');
+
+    return ListTile(
+      title: Text(
+        item,
+        style: const TextStyle(fontSize: 18),
+      ),
+      trailing: IconButton(
+          onPressed: () {
+            ref.read(itemListProvider.notifier).deleteItem(item);
+          },
+          icon: const Icon(Icons.delete)),
     );
   }
 }
